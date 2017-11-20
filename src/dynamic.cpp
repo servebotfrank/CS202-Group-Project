2d210f3c70666777ea3cdefcd57d37283fe917e5
Bradley <bmmorton@alaska.edu>
2017-11-19 16:19:53 -0900

    This iteration has basic movement capabilites, though it still does not have capacity to take input. The issue I am worried about is that the collision checker reverts back the last position, and because heights won't be nice numbers after jumping I believe that with this model landing on an elevated object will be possible as values will simply revert back to their prior values, which would be a horrible infinite loop. I don't see a clear solution, but given the curren level of functionality I feel this is worth a push.

--- src/dynamic.cpp
+++ src/dynamic.cpp
@@ -14,7 +14,18 @@ double Dynamic_object::incrementPosition()
 {
 	double _tempx=_xPosition, _tempy=_yPosition;
 	_xPosition+=_timingInterval*velocity.getXDirection();
-
+	_yPosition+=_timingInterval*velocity.getYDirection();
+	int _tempPosX=_xPosition;
+	if(_yPosition<_elevations[_tempPosX])
+	{
+		_xPosition=_tempx;
+		_yPosition=_tempy;
+		velocity.setXDirection(0);
+	}
+	if(_yPosition>_elevations[_tempPosX])
+	{
+		velocity.setYDirection(velocity.getYDirection()-9.801/_timingInterval);
+	}
 
 	//add in changes to x and y velocities
 
@@ -25,8 +36,8 @@ void Dynamic_object::fixMomentum()
 	_momentum=velocity.getTotalVelocity()*_mass;
 }
 
-Dynamic_object::Dynamic_object(std::ostream & os, std::vector<double> & vec)
+Dynamic_object::Dynamic_object(std::ostream & os, const std::vector<double> & vec)
 {
 	_mass=_xPosition=_yPosition=_momentum=0;
-
+	_elevations=vec;
 }
--- src/dynamic.hpp
+++ src/dynamic.hpp
@@ -8,17 +8,18 @@
 class Dynamic_object
 {
 private:
+	bool _overElevationFlag;
 	Velocity velocity;
 	double _mass, _momentum, _xPosition, _yPosition;
 	const double _timingInterval=1/30;//set to whatever the timing interval is
-
+	std::vector<double> _elevations;
 	//Position x and y coords will talk to the graphics processor
 public:
 	void setMass(double mass);
 	double getMass()const;
 	double incrementPosition();
 	void fixMomentum();
-	Dynamic_object(std::ostream & os, std::vector<double> & vec);
+	Dynamic_object(std::ostream & os, const std::vector<double> & vec);
 };
 
 #endif
